fd4c328da24f9a118bf312008cfbe88ce8c73024
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2021-12-02 09:29:44 -0500

    Slide was made interactive, attempted to make button functional, created value variable.

--- GuessingGame/ContentView.swift
+++ GuessingGame/ContentView.swift
@@ -8,14 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: Stored properties
+    // The ccurrent guess of the user
+    @State var currentGuess: Double = 50
+    @State var value: Double = 50
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            VStack {
+                Slider(value: $currentGuess,
+                       in: 0...100,
+                       step: 1,
+                       label: {
+                    Text("Opacity")
+                },
+                       minimumValueLabel: {
+                    Text("0")
+                },
+                       maximumValueLabel: {
+                    Text("100")
+                })
+                Text("\(String(format: "%.0f", currentGuess))")
+                Button(action: {
+                    if(value == currentGuess) {
+                        value = Double(Int.random(in: 1..<100))
+                        print("You have guessed number!")
+                    }
+                    else {
+                        if(value > currentGuess) {
+                            print("Higher")
+                        }
+                        else {
+                            print("Lower")
+                        }
+                    }
+                    print("Button was pressed")
+                }, label: {
+                    Text("Submit Guess")
+                })
+                    .buttonStyle(.bordered)
+            }
+        }
+        .padding()
+        .navigationTitle("Guessing Game")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView {
+            ContentView()
+        }
     }
 }
